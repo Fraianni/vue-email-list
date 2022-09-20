410a03081d75533b84c9274e3dc8e15c0a974705
Fraianni <francescoianni1995@gmail.com>
2022-09-20 14:29:41 +0200

    completo senza bonus

---
+++ script/script.js
@@ -0,0 +1,32 @@
+console.log('JS OK!');
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        eMail: [],
+        limit: 10,
+
+    },
+    methods: {
+        generateMail() {
+            console.log(this.eMail);
+
+            this.eMail = [];
+            for (let i = 1; i <= this.limit; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+
+                    .then((response) => {
+                        if (response.status === 200) {
+                            const randomMail = response.data.response;
+                            //  this.numbers = [randomNumber];
+                            this.eMail.push(randomMail);
+                        }
+                    });
+            }
+            console.log(this.eMail);
+
+
+        },
+    },
+});
